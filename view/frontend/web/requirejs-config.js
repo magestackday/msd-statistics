9891d3fc343f1980b4a606cf93a6e44c25ce8c1c
Anna Voelkl <anna@voelkl.at>
2016-01-09 22:21:36 +0100

    Update requirejs config

--- view/frontend/web/requirejs-config.js
+++ view/frontend/web/requirejs-config.js
@@ -1,9 +1,9 @@
 var config = {
     map: {
         '*': {
-            amCharts:           'Msd_Statistics/amcharts/amcharts',
-            amChartsNone:       'Msd_Statistics/amcharts/none',
-            amChartsSerial:     'Msd_Statistics/amcharts/serial',
+            amCharts:           'Msd_Statistics/js/amcharts/amcharts',
+            amChartsNone:       'Msd_Statistics/js/amcharts/none',
+            amChartsSerial:     'Msd_Statistics/js/amcharts/serial'
         }
     }
 };
